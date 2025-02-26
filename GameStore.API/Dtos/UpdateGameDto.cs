dfedb808c7a96df212a75eba07f2c72c81fb7a0c
Marcelo Vesantera <mrvesantera@hotmail.com>
2025-02-26 20:25:17 -0300

    validações + nuget MinimalAPI

--- GameStore.API/Dtos/UpdateGameDto.cs
+++ GameStore.API/Dtos/UpdateGameDto.cs
@@ -1,9 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.API.Dtos;
 
 public record class UpdateGameDto(
+    [Required]
+    [StringLength(50)]
     string Name,
+
+    [Required]
+    [StringLength(20)]
     string Genre,
+
+    [Required]
+    [StringLength(300)]
     string Description,
+
+    [Required]
+    [Range(1, 100)]
     decimal Price,
+
+    [Required]
     DateOnly ReleaseDate
 );
