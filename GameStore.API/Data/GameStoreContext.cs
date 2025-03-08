accd7cefbfe4769d9f2eb1d82c266cae125c5666
Marcelo Vesantera <mrvesantera@hotmail.com>
2025-03-08 15:12:46 -0300

    initial data for Genre table

--- GameStore.API/Data/GameStoreContext.cs
+++ GameStore.API/Data/GameStoreContext.cs
@@ -7,4 +7,15 @@ public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbCo
 {
     public DbSet<Game> Games => Set<Game>();
     public DbSet<Genre> Genres => Set<Genre>();
+
+    override protected void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new { Id = 1, Name = "RPG" },
+            new { Id = 2, Name = "Sports" },
+            new { Id = 3, Name = "Action" },
+            new { Id = 4, Name = "Racing" },
+            new { Id = 5, Name = "Fighting" }
+        );
+    }
 }
