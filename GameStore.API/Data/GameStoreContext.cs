c85d7d13c0b40dad560b4a3dd067b2e862926e35
Marcelo Vesantera <mrvesantera@hotmail.com>
2025-03-09 13:45:44 -0300

    func: seed games data at run

--- GameStore.API/Data/GameStoreContext.cs
+++ GameStore.API/Data/GameStoreContext.cs
@@ -10,6 +10,14 @@ public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbCo
 
     override protected void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Game>().HasData(
+            new{ Id = 1, Name = "The Witcher 3", GenreId = 1, Description = "The Witcher 3: Wild Hunt is a story-driven open world RPG set in a visually stunning fantasy universe full of meaningful choices and impactful consequences.", Price = 29.99m, ReleaseDate = new DateOnly(2015, 5, 19)},
+            new{ Id = 2, Name = "Cyberpunk 2077", GenreId = 1, Description = "Cyberpunk 2077 is an open-world, action-adventure story set in Night City, a megalopolis obsessed with power, glamour and body modification.", Price = 59.99m, ReleaseDate = new DateOnly(2020, 12, 10)},
+            new{ Id = 3, Name = "The Elder Scrolls V: Skyrim", GenreId = 1, Description = "The Elder Scrolls V: Skyrim is an open world action role-playing video game developed by Bethesda Game Studios and published by Bethesda Softworks.", Price = 19.99m, ReleaseDate = new DateOnly(2011, 11, 11)},
+            new{ Id = 4, Name = "Grand Theft Auto V", GenreId = 3, Description = "Grand Theft Auto V is a 2013 action-adventure game developed by Rockstar North and published by Rockstar Games.", Price = 29.99m, ReleaseDate = new DateOnly(2013, 9, 17)},
+            new{ Id = 5, Name = "Red Dead Redemption 2", GenreId = 3, Description = "Red Dead Redemption 2 is a 2018 action-adventure game developed and published by Rockstar Games.", Price = 39.99m, ReleaseDate = new DateOnly(2018, 10, 26)}
+        );
+
         modelBuilder.Entity<Genre>().HasData(
             new { Id = 1, Name = "RPG" },
             new { Id = 2, Name = "Sports" },
---
+++ GameStore.API/Data/Migrations/20250309164332_SeedGamesData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GameStore.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedGamesData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Games",
+                columns: new[] { "Id", "Description", "GenreId", "Name", "Price", "ReleaseDate" },
+                values: new object[,]
+                {
+                    { 1, "The Witcher 3: Wild Hunt is a story-driven open world RPG set in a visually stunning fantasy universe full of meaningful choices and impactful consequences.", 1, "The Witcher 3", 29.99m, new DateOnly(2015, 5, 19) },
+                    { 2, "Cyberpunk 2077 is an open-world, action-adventure story set in Night City, a megalopolis obsessed with power, glamour and body modification.", 1, "Cyberpunk 2077", 59.99m, new DateOnly(2020, 12, 10) },
+                    { 3, "The Elder Scrolls V: Skyrim is an open world action role-playing video game developed by Bethesda Game Studios and published by Bethesda Softworks.", 1, "The Elder Scrolls V: Skyrim", 19.99m, new DateOnly(2011, 11, 11) },
+                    { 4, "Grand Theft Auto V is a 2013 action-adventure game developed by Rockstar North and published by Rockstar Games.", 3, "Grand Theft Auto V", 29.99m, new DateOnly(2013, 9, 17) },
+                    { 5, "Red Dead Redemption 2 is a 2018 action-adventure game developed and published by Rockstar Games.", 3, "Red Dead Redemption 2", 39.99m, new DateOnly(2018, 10, 26) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
